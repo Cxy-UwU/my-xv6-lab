81dcd60fbed60152eb21979a53d863698b587559
Cxy-UwU <2867747371@qq.com>
2024-07-18 16:25:26 +0800

    refine pingpong: 缺少了子进程给父进程发送字节的过程，居然没有被打分程序发现，回头写报告才发现问题。本次commit对这个问题进行了修复。

--- user/pingpong.c
+++ user/pingpong.c
@@ -13,19 +13,23 @@ int main()
 
     if (fork() == 0) // child
     {
-        close(p[1]); // close write
+        close(p[1]); // close write end
         char temp;
         read(p[0], &temp, 1);
         printf("%d: received ping\n", getpid());
-        close(p[0]); // close read
+        write(p[0], &temp, 1); // write back to parent
+        close(p[0]);           // close read end
+        exit(0);
     }
     else
     {
-        close(p[0]); // close read
+        close(p[0]); // close read end
         char temp = 'x';
         write(p[1], &temp, 1);
-        close(p[1]); // close write
+        close(p[1]); // close write end
         wait((int *)0);
+        close(p[1]);          // close write end
+        read(p[0], &temp, 1); // read from child
         printf("%d: received pong\n", getpid());
     }
 
