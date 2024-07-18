b8269bee56f528e57b1b5bff5ae19ae018d12a3c
Cxy-UwU <2867747371@qq.com>
2024-07-18 12:08:44 +0800

    find

---
+++ user/find.c
@@ -0,0 +1,96 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
+
+/*
+从给定的路径中提取出文件名，并将文件名格式化为固定长度
+这个函数是参考xv6自带的 ls.c 实现的。
+本函数在 find 中被调用
+*/
+char *fmtname(char *path)
+{
+    static char buf[DIRSIZ + 1];
+    char *p;
+
+    // Find first character after last slash.
+    for (p = path + strlen(path); p >= path && *p != '/'; p--)
+        ;
+    p++;
+
+    // Return blank-padded name.
+    if (strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    memset(buf + strlen(p), ' ', DIRSIZ - strlen(p));
+    return buf;
+}
+
+/*
+查找文件
+如果是文件夹，则函数递归地调用自身，继续向内层搜索
+如果是文件，则通过strcmp比较文件名是否一致。
+*/
+void find(char *path, char *key)
+{
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if ((fd = open(path, O_RDONLY)) < 0)
+    {
+        fprintf(2, "find: cannot open %s\n", path);
+        return;
+    }
+
+    if (fstat(fd, &st) < 0)
+    {
+        fprintf(2, "find: cannot stat %s\n", path);
+        close(fd);
+        return;
+    }
+
+    switch (st.type)
+    {
+    case T_FILE:
+        if (strcmp(fmtname(path), key) != 0)
+        {
+            printf("%s\n", path);
+        }
+        break;
+
+    case T_DIR:
+        if (strlen(path) + 1 + DIRSIZ + 1 > sizeof(buf))
+        {
+            printf("find: path too long\n");
+            break;
+        }
+        strcpy(buf, path);
+        p = buf + strlen(buf);
+        *p++ = '/';
+        while (read(fd, &de, sizeof(de)) == sizeof(de))
+        {
+            if (de.inum == 0 || strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            find(buf, key);
+        }
+        break;
+    }
+    close(fd);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc <= 2)
+    {
+        printf("too few arguments! \nusage: find [path] [key]\n");
+        exit(1);
+    }
+
+    find(argv[1], argv[2]);
+    exit(0);
+}
